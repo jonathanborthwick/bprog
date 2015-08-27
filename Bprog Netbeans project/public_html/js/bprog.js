f6b55df2993b9e6f39541ad7230f912ebb4e4c9c
Jonathan Borthwick <jborthwickog@gmail.com>
2015-08-26 21:47:57 -0700

    Binding example data using module and controller, using return context from function syntax

--- Bprog Netbeans project/public_html/js/bprog.js
+++ Bprog Netbeans project/public_html/js/bprog.js
@@ -1,11 +1,32 @@
 var bprog = bprog || {}
 bprog.angulartests = bprog.angulartests || {}
-
-bprog.angulartests.data = function(){
+bprog.angulartests.data = function () {
     var context = {};
-    context.people=[{name:'Jono',city:'langley'},{name:'Andrew',city:'bearsden'}];
+    context.people = [{name: 'Jono', city: 'langley'}, {name: 'Andrew', city: 'bearsden'}];
+    context.bprogModule = angular.module('bprogModule', []);
+    context.bprogModule.controller('CustomersController', function () {
+        this.sortBy = 'name';
+        this.reverse = false;
+        this.customers = [
+            {
+                joined: '2015-08-26', name: 'me', city: 'Langley'
+            },
+            {
+                joined: '2015-08-25', name: 'you', city: 'Bearsden'
+            }
+        ];
+        this.doSort = function (propName) {
+            this.sortBy = propName;
+            this.reverse = !this.reverse;
+        }
+    });
     return context;
-}();
+}();//self call
+
+
+
+
+
 
 
 
